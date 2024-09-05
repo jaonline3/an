273d9c4704e2d0e0c25fe27c66627345733e0951
jaonline1 <147401220+jaonline1@users.noreply.github.com>
2024-09-05 17:51:28 +0500

    Update pl3.py

--- pl3.py
+++ pl3.py
@@ -16,7 +16,7 @@ def generate_unique_filename(base_name):
     return f"{base_name}_{timestamp}.json"
 
 # Initialize Box client with your access token
-ACCESS_TOKEN = 'lXD0r8yCkI6j5YafLsdwyuMpKC6I1Lir'
+ACCESS_TOKEN = 'ylt7qnRll7FbGabipJ2cdJQERDsh56jY'
 oauth2 = OAuth2(client_id=None, client_secret=None, access_token=ACCESS_TOKEN)
 client = Client(oauth2)
 
